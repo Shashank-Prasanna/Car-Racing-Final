05f3e86fc6bdb9e229109e37d4e0cdd4458d6ae3
SpyGuy <shashankprasanna1@gmail.com>
2020-12-12 17:47:42 -0500

    added files

--- js/Game.js
+++ js/Game.js
@@ -1,84 +1,98 @@
 class Game {
-  constructor(){
+	constructor() {}
 
-  }
+	getState() {
+		var gameStateRef = database.ref('gameState');
+		gameStateRef.on('value', function (data) {
+			gameState = data.val();
+		});
+	}
 
-  getState(){
-    var gameStateRef  = database.ref('gameState');
-    gameStateRef.on("value",function(data){
-       gameState = data.val();
-    })
+	update(state) {
+		database.ref('/').update({
+			gameState: state,
+		});
+	}
 
-  }
+	async start() {
+		if (gameState === 'Lobby') {
+			player = new Player();
+			var playerCountRef = await database.ref('playerCount').once('value');
+			if (playerCountRef.exists()) {
+				playerCount = playerCountRef.val();
+				player.getCount();
+			}
+			form = new Form();
+			form.display();
+		}
 
-  update(state){
-    database.ref('/').update({
-      gameState: state
-    });
-  }
+		car1 = createSprite(100, 200);
+		car1.addImage(image1);
+		car2 = createSprite(300, 200);
+		car2.addImage(image2);
+		car3 = createSprite(500, 200);
+		car3.addImage(image3);
+		car4 = createSprite(700, 200);
+		car4.addImage(image4);
+		cars = [car1, car2, car3, car4];
+	}
 
-  async start(){
-    if(gameState === 0){
-      player = new Player();
-      var playerCountRef = await database.ref('playerCount').once("value");
-      if(playerCountRef.exists()){
-        playerCount = playerCountRef.val();
-        player.getCount();
-      }
-      form = new Form()
-      form.display();
-    }
+	play() {
+		form.hide();
 
-    car1 = createSprite(100,200);
-    car2 = createSprite(300,200);
-    car3 = createSprite(500,200);
-    car4 = createSprite(700,200);
-    cars = [car1, car2, car3, car4];
-  }
+		Player.getPlayerInfo();
 
-  play(){
-    form.hide();
+		if (allPlayers !== undefined) {
+			//var display_position = 100;
 
-    Player.getPlayerInfo();
-    
-    if(allPlayers !== undefined){
-      //var display_position = 100;
-      
-      //index of the array
-      var index = 0;
+			//index of the array
+			var index = 0;
 
-      //x and y position of the cars
-      var x = 0;
-      var y;
+			//x and y position of the cars
+			var x = 200;
+			var y;
+			background(imageGround);
+			image(imageTrack, 0, -displayHeight * 4, displayWidth, displayHeight * 5);
 
-      for(var plr in allPlayers){
-        //add 1 to the index for every loop
-        index = index + 1 ;
+			for (var plr in allPlayers) {
+				//add 1 to the index for every loop
+				index = index + 1;
 
-        //position the cars a little away from each other in x direction
-        x = x + 200;
-        //use data form the database to display the cars in y direction
-        y = displayHeight - allPlayers[plr].distance;
-        cars[index-1].x = x;
-        cars[index-1].y = y;
+				//position the cars a little away from each other in x direction
+				x = x + 300;
+				//use data form the database to display the cars in y direction
+				y = displayHeight - allPlayers[plr].distance;
+				cars[index - 1].x = x;
+				cars[index - 1].y = y;
 
-        if (index === player.index){
-          cars[index - 1].shapeColor = "red";
-          camera.position.x = displayWidth/2;
-          camera.position.y = cars[index-1].y
-        }
-       
-        //textSize(15);
-        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
-      }
+				//textSize(15);
+				//text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
 
-    }
+				if (index === player.index) {
+					fill('#FF0000');
+					//textSize(50);
+					ellipse(x, y, 70);
+					//text(player.name, x - 50, y + 50);
+					console.log(player.name);
+					cars[index - 1].shapeColor = 'red';
+					camera.position.x = displayWidth / 2;
+					camera.position.y = cars[index - 1].y;
+				}
+			}
+		}
 
-    if(keyIsDown(UP_ARROW) && player.index !== null){
-      player.distance +=10
-      player.update();
-    }
+		if (keyIsDown(UP_ARROW) && player.index !== null) {
+			player.distance += 10;
+			player.update();
+		}
 
-    drawSprites();
-  }
+		drawSprites();
+		if (player.distance > 5189) {
+			gameState = 'End';
+		}
+	}
+
+	end() {
+		console.log('Ended!');
+	}
 }
--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var canvas, backgroundImage;
 
-var gameState = 0;
+var gameState = 'Lobby';
 var playerCount;
 var allPlayers;
 var distance = 0;
@@ -9,23 +9,34 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var image1, image2, image3, image4, imageTrack, imageGround;
 
-
-function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
-  database = firebase.database();
-  game = new Game();
-  game.getState();
-  game.start();
+function preload() {
+	image1 = loadImage('images/car1.png');
+	image2 = loadImage('images/car2.png');
+	image3 = loadImage('images/car3.png');
+	image4 = loadImage('images/car4.png');
+	imageTrack = loadImage('images/track.jpg');
+	imageGround = loadImage('images/ground.png');
 }
 
+function setup() {
+	canvas = createCanvas(displayWidth - 20, displayHeight - 30);
+	database = firebase.database();
+	game = new Game();
+	game.getState();
+	game.start();
+}
 
-function draw(){
-  if(playerCount === 4){
-    game.update(1);
-  }
-  if(gameState === 1){
-    clear();
-    game.play();
-  }
+function draw() {
+	if (playerCount === 4) {
+		game.update('Play');
+	}
+	if (gameState === 'Play') {
+		clear();
+		game.play();
+	}
+	if (gameState === 'End') {
+		game.end();
+	}
 }
